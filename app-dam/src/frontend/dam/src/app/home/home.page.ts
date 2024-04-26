340781ed2877ae5a9d509da2ea1fa2ce3319e5a0
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2024-04-26 16:09:07 -0300

    se pueden visualizar los sensores y una primera version de las tarjetas

--- app-dam/src/frontend/dam/src/app/home/home.page.ts
+++ app-dam/src/frontend/dam/src/app/home/home.page.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DispositivoService } from '../services/dispositivo.service';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+//
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-})
-export class HomePage {
 
-  constructor() {}
+})
+export class HomePage implements OnInit {
+
+  listaDispositivos: any[] = [];
+  listaMediciones: any[] = [];
+  isLoading: boolean = false;
+  mostrarGrafico: boolean = false;
+  dispositvoSeleccionado!: number;
+
+  constructor(private servicio: DispositivoService, private http: HttpClient) {}
+
+  ngOnInit(){
+    this.listarDispositivos();
+  }
+
+  listarDispositivos(){
+    this.isLoading = true;
+    this.servicio.getDispositivos().subscribe((data: any) => {
+      this.listaDispositivos = data;
+      this.isLoading = false;
+    });
+  }
+
+  mostrarSensor(idDispositivo: number){
+    this.dispositvoSeleccionado = idDispositivo;
+    this.servicio.getMediciones().subscribe((data: any) => {
+      this.listaMediciones = data;
+      this.listaMediciones = this.listaMediciones.filter(m => m.dispositivoId === idDispositivo);
+      console.log(this.listaMediciones)
+      this.generarGrafico();
+      this.mostrarGrafico = true;
+    });
+  }
+  generarGrafico() {
+    console.log("mostrar grafico");
+  }
+
+  switchElectrovalvula() {
+    console.log("abrir o cerrar electrovalvula");
+  }
+
+  verMediciones() {
+    console.log("ver mediciones");
+  }
+
+  verLogs() {
+    console.log("ver logs");
+  }
 
 }
---
+++ app-dam/src/frontend/dam/src/app/services/dispositivo.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, firstValueFrom, map } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DispositivoService {
+
+  constructor(private _http: HttpClient) { }
+
+  getDispositivos () {
+    return this._http.get('http://localhost:8000/dispositivos').pipe(map((data : any)=> {
+        return data;
+    }))
+  }
+
+  getMediciones (): Observable<any>  {
+    return this._http.get('http://localhost:8000/mediciones').pipe(map((data : any)=> {
+        return data;
+    }))
+  }
+
+  getLogs (): Observable<any>  {
+    return this._http.get('http://localhost:8000/logriegos').pipe(map((data : any)=> {
+        return data;
+    }))
+  }
+}
